87e5c439742273584a9f12f29b25d242bc2e3914
Petec0x0 <udehonyedikachi01@gmail.com>
2022-09-10 18:18:40 +0100

    Removed stats, fixed redirects for frontend production

--- client/src/components/Stats.js
+++ client/src/components/Stats.js
@@ -4,7 +4,7 @@ const Stats = () => {
     return (
         <div className="bg-teal-50 flex-wrap py-2 px-10">
             <div className="flex flex-col justify-evenly md:flex-row">
-                <div className="flex items-center justify-evenly p-6 bg-white w-64 rounded-xl mx-4 mt-2 shadow-lg md:mx-2">
+                {/* <div className="flex items-center justify-evenly p-6 bg-white w-64 rounded-xl mx-4 mt-2 shadow-lg md:mx-2">
                     <div>
                         <span className="text-sm font-semibold text-gray-400">Total Sales</span>
                         <h1 className="text-2xl font-bold">$682.5</h1>
@@ -47,7 +47,7 @@ const Stats = () => {
                             <path fillRule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clipRule="evenodd" />
                         </svg>
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>
     )
